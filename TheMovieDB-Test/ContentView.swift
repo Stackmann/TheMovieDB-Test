758a9eabd28344f32be172c477800d52f43959cf
Oleg <33181376+Stackmann@users.noreply.github.com>
2021-05-25 08:33:10 +0300

    The first commit

---
+++ TheMovieDB-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheMovieDB-Test
+//
+//  Created by admin on 25.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Lets start!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheMovieDB-Test/TheMovieDB_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDB_TestApp.swift
+//  TheMovieDB-Test
+//
+//  Created by admin on 25.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDB_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
